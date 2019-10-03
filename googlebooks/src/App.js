3416dcb863643f2b0fb9ec4120f3ba8f65e701aa
Lrop <Lagropner04@gmail.com>
2019-10-03 15:02:14 -0600

    error

--- googlebooks/src/App.js
+++ googlebooks/src/App.js
@@ -1,25 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Search from "./pages/Search";
-import Saved from "./pages/Saved";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SaveBooks from "./pages/SaveBooks";
+import SearchBooks from "./pages/SearchBooks";
+import NoMatch from "./pages/NoMatch";
 import NavBar from "./components/NavBar";
-import('normalize.css');
+import Footer from "./components/Footer"
+import "./App.css"
+
 
 function App() {
-      return (
-
-            <Router>
-                  <div>
-                        <NavBar />
-
-                        <Switch>
-                              <Route exact path="/" component={Search}>Search</Route>
-                              <Route path="/saved" component={Saved}>Saved</Route>
-                        </Switch>
-                  </div>
-            </Router>
-      );
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={SearchBooks} />
+          <Route exact path="/saved" component={SaveBooks} />
+          <Route exact path="/saved/:id" component={SaveBooks} />
+          <Route component={NoMatch} /> 
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- googlebooks/src/components/NavBar/index.js
+++ googlebooks/src/components/NavBar/index.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
 import "./navbar.css";
 
-function NavBar() {
+
+function Nav() {
     return (
-
-        <nav className="nav-container">
-            <ul className="nav-list">
-
-                <li><a className="nav-link" href="https://books.google.com/" target="_blank" rel="noopener noreferrer">Google Books</a></li>
-                <li><Link to="/" className="nav-link">Search</Link></li>
-                <li><Link to="/saved" className="nav-link">Saved</Link></li>
-            </ul>
+        <nav className="navbar navbar-expand-lg navbar-primary">
+            <a className="navbar-brand" href="/">
+                <h2 className="text-white">Google Book Search</h2>
+            </a>
+            <div id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item" id="home">
+                        <a className="nav-link" href="/"><button type="button" className="btn btn-info text-white">Search Books</button></a>
+                    </li>
+                    <li className="nav-item" id="report">
+                        <a className="nav-link" href="/saved"><button type="button" className="btn btn-warning text-white">Saved Books</button></a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 }
 
-export default NavBar;
+export default Nav;
