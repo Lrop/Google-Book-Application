ae7f138d6ce0e26fcc2851c8e6b8f799b1b09cdb
Lrop <Lagropner04@gmail.com>
2019-10-03 16:05:22 -0600

    commiting working files

--- googlebooks/src/components/NavBar/index.js
+++ googlebooks/src/components/NavBar/index.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-
 import "./navbar.css";
 
 function NavBar() {
@@ -17,30 +14,8 @@ function NavBar() {
                 <br></br>
                 <li className="button-two"><Link to="/saved" className="nav-link">Saved</Link></li>
             </ul>
-=======
-import React from "react";
-import "./navbar.css";
-
-
-function Nav() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-primary">
-            <a className="navbar-brand" href="/">
-                <h2 className="text-white">Google Book Search</h2>
-            </a>
-            <div id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item" id="home">
-                        <a className="nav-link" href="/"><button type="button" className="btn btn-info text-white">Search Books</button></a>
-                    </li>
-                    <li className="nav-item" id="report">
-                        <a className="nav-link" href="/saved"><button type="button" className="btn btn-warning text-white">Saved Books</button></a>
-                    </li>
-                </ul>
-            </div>
->>>>>>> 7e7382eebc49543efa45b44a81fa47332756affc
         </nav>
     );
 }
 
-export default Nav;
+export default NavBar;
